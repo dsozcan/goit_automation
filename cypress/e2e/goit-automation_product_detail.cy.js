3079b4d97226787ed1667e631cc2621e5e80d171
Damla Sevin <damlasevin@gmail.com>
2025-09-26 15:31:00 +0300

    product details page test released

---
+++ cypress/e2e/goit-automation_product_detail.cy.js
@@ -0,0 +1,21 @@
+import product_detail from "../pages/product_detail"
+
+//ignore any error caused by the application
+Cypress.on('uncaught:exception', (err, runnable) => {
+console.error('Uncaught exception:', err.message);
+return false;
+});
+
+//open url and login before tests (function defined in support)
+describe("Product deatil page tests", () => {
+  
+  before(function () {
+    cy.login();
+  })
+
+  it('TC01 - Product detail page elemnts check', () => {
+    product_detail.selectBook();
+    product_detail.addCart();
+  })
+
+});
---
+++ cypress/pages/product_detail.js
@@ -0,0 +1,54 @@
+class detail{
+
+productCard = '.image-wrapper';
+productTitle = '#product-title';
+productWriter = '#model-title > span';
+productPublisher = '#brand-title';
+productPrice = '.product-price';
+productInformation = '.col-12.mt-1 > .row';
+addCartBtn = '#addToCartBtn';
+popUp = '#popup-cart';
+popUpClose = '#t-modal-close-1 > .ti-close';
+cartItem = '.badge';
+
+selectBook () {
+    cy.get(this.productCard).first().click();
+        cy.get(this.productTitle).should('be.visible');
+        cy.get(this.productWriter).should('be.visible');
+        cy.get(this.productPublisher).should('be.visible');
+        cy.get(this.productPrice).should('be.visible');
+        cy.get(this.productInformation).should('contain.text', 'Türü')
+        .and('contain.text', 'ISBN')
+        .and('contain.text', 'Kapak')
+        .and('contain.text', 'Basım Yılı')
+        .and('contain.text', 'Sayfa Sayısı')
+        .and('contain.text', 'Kağıt Tipi');
+}
+
+addCart () {
+
+  cy.get('.badge.cart-soft-count')
+    .invoke('text')
+    .then((text) => {
+      const initialCount = parseInt(text.trim(), 10) || 0;
+
+        cy.get(this.addCartBtn).click().should('be.visible');
+        cy.wait(2000);
+        cy.get(this.popUp).should('exist');
+        cy.get(this.popUpClose).click();
+        cy.log(cy.get(this.cartItem));
+
+      // 3️⃣ Sepetteki yeni sayıyı kontrol et
+      cy.get('.badge.cart-soft-count', { timeout: 10000 })
+        .invoke('text')
+        .then((newText) => {
+          const newCount = parseInt(newText.trim(), 10);
+          expect(newCount).to.eq(initialCount + 1);
+        });
+    });
+
+}
+
+}
+
+export default new detail();
