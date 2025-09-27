221ed3bcfb6908d658bcd3e8790de074141ed0fe
Damla Sevin <damlasevin@gmail.com>
2025-09-27 17:24:28 +0300

    guest page tests completed

---
+++ cypress/e2e/goit_automatio_guest.cy.js
@@ -0,0 +1,31 @@
+import user from "../fixtures/user.json"
+import login from "../pages/login"
+import guest from "../pages/guest"
+
+//ignore any error caused by the application
+Cypress.on('uncaught:exception', (err, runnable) => {
+console.error('Uncaught exception:', err.message);
+return false;
+});
+
+//open url and login before tests (function defined in support)
+describe("Product deatil page tests", () => {
+  
+  beforeEach(function () {
+    cy.visit(user.url_homepage);
+    cy.get('body').then(($body) => {
+      const $btn = $body.find(login.cookieAcceptField);
+      if ($btn.length && $btn.is(':visible')) {
+        cy.wrap($btn).click();
+      }
+    });
+  });
+
+    it('TC01 - Cart Information Page Information Check', () => {
+        guest.openPurchasePage();
+        guest.continueAsGuest();
+        guest.adressFormCheck();
+        guest.emptyFormError();
+        guest.fillAdressForm();
+  })
+})
---
+++ cypress/pages/guest.js
@@ -0,0 +1,58 @@
+class guest{
+
+    purchasePageBtn = '#cart-popup-continue-shopping';
+    continueAsGuestBtn = '#membership-form-131 > .w-100';
+    adressForm = '#order-address-form';
+    saveAdressBtn = '.col-12 > .btn';
+    emptyFormMessage = '.popover-item';
+    formName = '[name="fullname"]';
+    formEmail = '[name="email"]';
+    formCity = '[name="city_code"]';
+    formCountry = '[name="country_code"]';
+    formTown = '[name="town_code"]';
+    formDistrict = '[name="district_code"]';
+    formAdress = '[name="address"]';
+    formPhone = '[name="mobile_phone"]';
+    saveFormBtn = '.col-12 > .btn';
+    cargoOptions = '.payment-cargo-list > .p-2 > .text-uppercase';
+
+openPurchasePage () {
+    cy.get('.product-item').first().realHover();
+    cy.get('.product-item').first().find('.add-to-cart-btn')
+    .should('be.visible').and('contain.text', 'Sepete Ekle').click();
+    cy.get(this.purchasePageBtn).click();
+}
+
+continueAsGuest () {
+    cy.get(this.continueAsGuestBtn).click();
+}
+
+adressFormCheck () {
+    cy.get(this.adressForm).should('contain.text', 'Ad Soyad')
+    .and('contain.text', 'E-Mail')
+    .and('contain.text', 'İl')
+    .and('contain.text', 'İlçe')
+    .and('contain.text', 'Mahalle')
+    .and('contain.text', 'Adres')
+}
+
+emptyFormError () {
+    cy.get(this.saveAdressBtn).click();
+    cy.get(this.emptyFormMessage).should('contain.text', 'Lütfen bu alanı doldurunuz');
+}
+
+fillAdressForm () {
+    cy.get(this.formName).type('abcdef abc');
+    cy.get(this.formEmail).type('bkh@mail.com');
+    cy.get(this.formCity).select("Adana");
+    cy.get(this.formCountry).select("Türkiye");
+    cy.get(this.formTown).select("Ceyhan");
+    cy.get(this.formDistrict).select("ADATEPE MAH");
+    cy.get(this.formAdress).type("abc11 abc11 abc11 abc11 abc11 abc11");
+    cy.get(this.formPhone).type("5555555555");
+    cy.get(this.saveFormBtn).click();
+    cy.get(this.cargoOptions).should('contain.text', 'Kargo Seçenekleri')
+}
+
+}
+export default new guest();
